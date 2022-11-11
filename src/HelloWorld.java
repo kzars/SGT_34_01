539a41694bc7b1ef84806f5cad244e3e72361599
Kārlis Zars <zarskarlis@gmail.com>
2022-11-11 21:17:09 +0200

    Comments, Variables, Declaration, Initialization

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello Kārlis");
+        System.out.println("Hello Kārlis"); // Printing my name
+        // This is comment
+        /*
+        This is
+        Multi-Line
+        Comment
+         */
     }
 }
---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Declaring new variable of type int (integer)
+        int number;
+
+        //Initialization of number variable
+        number = 10;
+
+        //Accessing number variables value
+        System.out.println(number);
+
+        //Declaring and initializing new variable at the same time
+        int age = 67;
+        System.out.println("age = " + age);
+    }
+}
